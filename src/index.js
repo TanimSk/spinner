ae6075a1b391c4860ad622ae579ac52d16489c97
tanimsk <sktanim5800@gmail.com>
2022-07-09 00:18:48 +0600

    hover

--- src/index.js
+++ src/index.js
@@ -7,70 +7,106 @@ import './index.css';
 
 class SpinnerWheel extends React.Component {
 
-  spinnerHovered = (event) =>{
-    console.log(event.type);
-  }
-
-  spinnerClicked = (event, arg) => {
+  isClicked = (event, arg) => {
     alert(arg);
   }
+
+  handleMouseOver = (e, data) => {
+    var hovered_size = this.size * 1.05; 
+    data.shape_data.graphics.clear().f("#181818")
+      .mt(0, 0)
+      .lt(Math.cos(this.angle * data.index) * hovered_size, Math.sin(this.angle * data.index) * hovered_size)
+      .arc(0, 0, hovered_size, data.index * this.angle, data.index * this.angle + this.angle)
+      .lt(0, 0);
+    this.stage.update(e);
+    console.log(5114);
+
+  }
+
+  handleMouseOut = (e, data) => {
+    data.shape_data.graphics.clear().f(data.color)
+      .mt(0, 0)
+      .lt(Math.cos(this.angle * data.index) * this.size, Math.sin(this.angle * data.index) * this.size)
+      .arc(0, 0, this.size, data.index * this.angle, data.index * this.angle + this.angle)
+      .lt(0, 0);
+    this.stage.update(e);
+    console.log(54);
+  }
+
 
   componentDidMount() {
     this.segments = [
       'Phaya Thai 2', 'Rutnin Eye Hospital', 'Samitivej', 'River Rihab',
+      'Phaya Thai 2', 'Rutnin Eye Hospital', 'Samitivej', 'River Rihab',
+      'Phaya Thai 2', 'Rutnin Eye Hospital', 'Samitivej', 'River Rihab',
+      'Phaya Thai 2', 'Rutnin Eye Hospital', 'Samitivej', 'River Rihab',
       'Bangkok Hospital \n(pattaya)', 'Vejthani', 'Bumrungrad', 'Miracle Asia Rehab', 'MedPark'
 
     ];
-    const size = 260;
-    const txt_size = 19;
-    const offset = 95;
+
+    this.size = 265;
+    const txt_size = 16;
+    const offset = 80;
+    this.total_segments = 20;
 
     const colors = [
-        '#76C695',
-        '#26B05F',
-        '#A570B0',
-        '#855CA6',
-        '#54CAF1',
-        '#269BD7',
-        '#F5CC4D',
-        '#F59B4F',
-        '#EC5857' 
-      ];
+      '#76C695',
+      '#26B05F',
+      '#A570B0',
+      '#855CA6',
+      '#76C695',
+      '#26B05F',
+      '#A570B0',
+      '#855CA6',
+      '#76C695',
+      '#26B05F',
+      '#A570B0',
+      '#855CA6',
+      '#76C695',
+      '#26B05F',
+      '#A570B0',
+      '#855CA6',
+      '#54CAF1',
+      '#269BD7',
+      '#F5CC4D',
+      '#F59B4F',
+      '#EC5857'
+    ];
 
 
     var canvas = ReactDOM.findDOMNode(
       document.getElementById('spin-wheel')
     );
 
-    var stage = new Stage(canvas);
-    stage.ena
+    this.stage = new Stage(canvas);
+    this.stage.enableMouseOver();
 
     Ticker.timingMode = Ticker.RAF;
 
     this.c = new Container();
-    
-    var angle = Math.PI * 2 / this.segments.length;
+
+    this.angle = Math.PI * 2 / this.total_segments;
 
     // Draw a wheel 
-    var angle_degree = 360 / this.segments.length;
+    var angle_degree = 360 / this.total_segments;
     var rotation_const = 0;
 
-    if (this.segments.length % 2 !== 0) rotation_const = angle_degree;
+    if (this.total_segments % 2 !== 0) rotation_const = angle_degree;
 
     var s = [];
 
-    for (var i = 0, l = this.segments.length; i < l; i++) {
+    for (var i = 0, l = this.total_segments; i < l; i++) {
       s[i] = new Shape();
       s[i].graphics.f(colors[i])
         .mt(0, 0)
-        .lt(Math.cos(angle * i) * size, Math.sin(angle * i) * size)
-        .arc(0, 0, size, i * angle, i * angle + angle)
+        .lt(Math.cos(this.angle * i) * this.size, Math.sin(this.angle * i) * this.size)
+        .arc(0, 0, this.size, i * this.angle, i * this.angle + this.angle)
         .lt(0, 0);
 
       // Add text child
       var new_cont = new Container();
 
-      new_cont.set({ regY: size - offset, rotation: (angle_degree * (i + 1) + rotation_const) - 5 });
+      new_cont.set({ regY: this.size - offset, rotation: (angle_degree * (i + 1) + rotation_const) - 5 });
 
       var txt = new Text(this.segments[i], (txt_size) + "px Arial", "#f5f5f5");
       txt.set({ rotation: 90, textAlign: "center" });
@@ -78,22 +114,30 @@ class SpinnerWheel extends React.Component {
       new_cont.addChild(txt);
 
       this.c.addChild(new_cont);
-      this.c.addChildAt(s[i], 0);
+      this.c.addChild(s[i]);
 
       s[i].on(
-        'click', (e)=>{
-          this.spinnerClicked(e, "your return msg")                       /***********  PASS ARGUMENT HERE!  ***********/
+        'click', (e) => {
+          this.isClicked(e, "your return msg")                       /***********  PASS ARGUMENT HERE!  ***********/
 
         }
-      ); 
+      );
+
+      s[i].on('mouseover', (e, data) => {
+        this.handleMouseOver(e, data);
+      }, null, false, { index: i, shape_data: s[i] });
+
+      s[i].on('mouseout', (e, data) => {
+        this.handleMouseOut(e, data);
+      }, null, false, { index: i, shape_data: s[i], color: colors[i] });
 
     }
 
-    this.c.x = this.c.y = size + 20;
-    this.c.cache(-size, -size, size * 2, size * 2);
+    this.c.x = this.c.y = this.size + 20;
+    // this.c.cache(-size, -size, size * 2, size * 2);
 
     this.c.rotation = 0;
-    stage.addChild(this.c);
+    this.stage.addChild(this.c);
 
     // Mode. 0=stopped, 1=moving, 2=stopping
     this.c.mode = 0;
@@ -101,24 +145,24 @@ class SpinnerWheel extends React.Component {
     this.rotation_trace = 0;
 
     Ticker.on("tick", (event) => {
-      stage.update(event);
+      this.stage.update(event);
     });
   }
 
-  rotate_wheel = () =>{
-    this.rotation_trace += 360 / this.segments.length;
+  rotate_wheel = () => {
+    this.rotation_trace += 360 / (this.total_segments / 2);
     Tween.get(this.c)
       .to({ rotation: this.rotation_trace }, 800, Ease.cubicOut)
-      .call(()=>{ this.c.mode = 0; });
+      .call(() => { this.c.mode = 0; });
   }
 
   render() {
     return (
       <div className='spin-container'>
         <canvas id="spin-wheel" width="280" height="280"></canvas>
-        <img className="spin-txt" src={spin} alt="Spin Wheel" onClick={this.rotate_wheel}/>
+        <img className="spin-txt" src={spin} alt="Spin Wheel" onClick={this.rotate_wheel} />
       </div>
-      
+
     );
   }
 }
